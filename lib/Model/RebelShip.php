aa588813dcb044bd69691494f286e52d0f49dfff
petrero <petrero@gmail.com>
2017-03-21 18:14:02 +0200

    Copy code from previous branch

---
+++ lib/Model/RebelShip.php
@@ -0,0 +1,10 @@
+<?php
+
+class RebelShip extends Ship{
+  public function getFavoriteJedi() {
+    $coolJedis = array('Yoda', 'Ben Kenobi');
+    $key = array_rand($coolJedis);
+    
+    return $coolJedis[$key];
+  }
+}
