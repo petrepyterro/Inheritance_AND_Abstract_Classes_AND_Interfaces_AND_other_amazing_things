26c97aa02b4d560a46f80b8eafaec2f8249d109c
petrero <petrero@gmail.com>
2017-03-21 21:25:31 +0200

    Copy code from previous branch

---
+++ lib/Model/RebelShip.php
@@ -0,0 +1,40 @@
+<?php
+
+class RebelShip extends Ship{
+  public function getFavoriteJedi() {
+    $coolJedis = array('Yoda', 'Ben Kenobi');
+    $key = array_rand($coolJedis);
+    
+    return $coolJedis[$key];
+  }
+  
+  public function getType() {
+   return 'Rebel';  
+  }
+  
+  public function isFunctional()
+  {
+    return true;
+  }
+  
+  public function getNameAndSpecs($useShortFormat = false)
+    {
+      if ($useShortFormat) {
+        return sprintf(
+          '%s: %s/%s/%s {Rebel}',
+          $this->getName(),
+          $this->getWeaponPower(),
+          $this->getJediFactor(),
+          $this->getStrength()
+        );
+      } else {
+        return sprintf(
+          '%s: w:%s, j:%s, s:%s {Rebel}',
+          $this->getName(),
+          $this->getWeaponPower(),
+          $this->getJediFactor(),
+          $this->getStrength()
+        );
+      }
+    }
+}
