0f21491fa135bb0d8707241ae35d1bc155a18df3
petrero <petrero@gmail.com>
2017-03-21 17:08:13 +0200

    Download code from knp

---
+++ bootstrap.php
@@ -0,0 +1,13 @@
+<?php
+
+$configuration = array(
+    'db_dsn'  => 'mysql:host=localhost;dbname=Services_Dependency_Injection_AND_Containers',
+    'db_user' => 'root',
+    'db_pass' => mysql,
+);
+
+require_once __DIR__.'/lib/Service/Container.php';
+require_once __DIR__.'/lib/Model/Ship.php';
+require_once __DIR__.'/lib/Service/BattleManager.php';
+require_once __DIR__.'/lib/Service/ShipLoader.php';
+require_once __DIR__.'/lib/Model/BattleResult.php';
