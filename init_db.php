5353ded11bdc2f699b8fa674532fc539e815a82c
petrero <petrero@gmail.com>
2017-03-21 20:25:00 +0200

    Modify database credentials and the name of the database.

--- init_db.php
+++ init_db.php
@@ -3,16 +3,16 @@
 /*
  * SETTINGS!
  */
-$databaseName = 'oo_battle';
+$databaseName = 'Services_Dependency_Injection_AND_Containers';
 $databaseUser = 'root';
-$databasePassword = '';
+$databasePassword = 'mysql';
 
 /*
  * CREATE THE DATABASE
  */
 $pdoDatabase = new PDO('mysql:host=localhost', $databaseUser, $databasePassword);
 $pdoDatabase->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-$pdoDatabase->exec('CREATE DATABASE IF NOT EXISTS oo_battle');
+$pdoDatabase->exec('CREATE DATABASE IF NOT EXISTS Services_Dependency_Injection_AND_Containers');
 
 /*
  * CREATE THE TABLE
